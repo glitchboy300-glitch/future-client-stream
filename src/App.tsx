d98e8e6618567baf5bd6ecdf310acf00e6440875
glitchboy300-glitch <glitchboy300@gmail.com>
2026-04-12 21:05:39 +0500

    Add /book route to App.tsx for Calendly booking page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Index from "./pages/Index";
+import Book from "./pages/Book";
 import Confirmed from "./pages/Confirmed";
 import NotFound from "./pages/NotFound";
 
@@ -19,6 +20,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="/book" element={<Book />} />
             <Route path="/confirmed" element={<Confirmed />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
